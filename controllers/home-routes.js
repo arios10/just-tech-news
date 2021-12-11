a860a800a3839d44760414c2c636143eeb83e4a5
arios10 <ariosfly3@gmail.com>
2021-12-11 16:11:46 -0700

    fixed home-routes.js .get('/') render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,10 +35,6 @@ router.get('/', (req, res) => {
         posts,
         loggedIn: req.session.loggedIn
       });
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
 });
 
 router.get('/login', (req, res) => {
